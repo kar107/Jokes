94ab82180ef2ecdadb0e362d0e2a110509eb305a
kar107 <ankitpatel99246@gmail.com>
2023-04-27 11:45:20 +0530

    jokes

--- project2.js
+++ project2.js
@@ -4,36 +4,37 @@
 const jokes = document.querySelector('#joke');
 const jokebtn = document.querySelector('#jokebtn');
 
-// const generatejokes = () =>{
-
-//     const setHeader = {
-//         headers: {
-//             Accept : "application/json"
-//         }
-//     }
-//     fetch('https://icanhazdadjoke.com/', setHeader)
-//     .then((res) => res.json())
-//     .then( (data) =>{
-//         jokes.innerHTML=data.joke;
-//     }).catch((error) =>{
-//         console.log(error);
-//     })
-// }
-
-const generatejokes = async () =>{
-    try{
-        const setHeader = {
-                    headers: {
-                        Accept : "application/json"
-                    }
-                }
-                const res = await fetch('https://icanhazdadjoke.com/', setHeader);
-                const data = await res.json();
-                jokes.innerHTML = data.joke;
-    }catch(err){
-        console.log('the error is'+err);
+const generatejokes = () =>{
+    jokes.innerHTML='loding...'
+    setTimeout(() => {
+    const setHeader = {
+        headers: {
+            Accept : "application/json"
+        }
     }
+    fetch('https://icanhazdadjoke.com/', setHeader)
+    .then((res) => res.json())
+    .then( (data) =>{
+        jokes.innerHTML=data.joke;
+    }).catch((error) =>{
+        console.log(error);
+    })}, 500);
 }
+
+// const generatejokes = async () =>{
+//     try{
+//         const setHeader = {
+//                     headers: {
+//                         Accept : "application/json"
+//                     }
+//                 }
+//                 const res = await fetch('https://icanhazdadjoke.com/', setHeader);
+//                 const data = await res.json();
+//                 jokes.innerHTML = data.joke;
+//     }catch(err){
+//         console.log('the error is'+err);
+//     }
+// }
 
 jokebtn.addEventListener('click',generatejokes);
 generatejokes();
