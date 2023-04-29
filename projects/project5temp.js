90cee3d7860f3d5d18ef1729ebc9db9cb63c9bc6
kar107 <ankitpatel99246@gmail.com>
2023-04-29 11:27:33 +0530

    jokes

--- projects/project5temp.js
+++ projects/project5temp.js
@@ -9,11 +9,16 @@ const calculatetemp =  () =>{
         let fahrenheit = Math.round((cel * 9/5) + 32);
         return fahrenheit;
     }
+    const fahTocel =(fah) =>
+    {
+        let celsius = Math.round((fah -32) * 5/9);
+        return celsius;
+    }
     let result ;
     if(valueselected=='cel')
     {
         result=celTofah(numbertemp);
-        document.getElementById('resultcontainer').innerHTML=`= ${result} fehrenheit`;
+        document.getElementById('resultcontainer').innerHTML=`= ${result} fahrenheit`;
     }else
     {
         result=fahTocel(numbertemp);
