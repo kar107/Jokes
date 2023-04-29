9943caf40ea3c372cc6fe99dee55cd5ef66e9f86
kar107 <ankitpatel99246@gmail.com>
2023-04-29 22:21:42 +0530

    jokes

--- projects/project5temp.js
+++ projects/project5temp.js
@@ -1,29 +1,25 @@
-const calculatetemp =  () =>{
+const calculateTemp =  () =>{
 
     const numbertemp = document.getElementById('temp').value;
     const tempselect = document.getElementById('temp1');
     const valueselected=temp1.option[tempselect.SelecteIndex].value;
 
-    const celTofah =(cel) =>
-    {
+    const celTofah =(cel) =>{
         let fahrenheit = Math.round((cel * 9/5) + 32);
         return fahrenheit;
     }
-    const fahTocel =(fah) =>
-    {
+    const fahTocel =(fah) =>{
         let celsius = Math.round((fah -32) * 5/9);
         return celsius;
     }
-    let result ;
+    let result;
     if(valueselected=='cel')
     {
-        result=celTofah(numbertemp);
+        result = celTofah(numbertemp);
         document.getElementById('resultcontainer').innerHTML=`= ${result} fahrenheit`;
     }else
     {
-        result=fahTocel(numbertemp);
+        result = fahTocel(numbertemp);
         document.getElementById('resultcontainer').innerHTML=`= ${result} celsius`;
     }
-
-
 }
